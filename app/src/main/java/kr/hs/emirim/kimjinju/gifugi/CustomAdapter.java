7aee11fba59b8fe4dfc0144761af355133bdffec
Jinju Kim <swinging13@naver.com>
2019-01-25 01:17:20 +0900

    기후기 어플 봄여름가을겨울 사계절로 축제 정리 (ListView 간격이 넓음 -> 해결못함)

---
+++ app/src/main/java/kr/hs/emirim/kimjinju/gifugi/CustomAdapter.java
@@ -0,0 +1,63 @@
+package kr.hs.emirim.kimjinju.gifugi;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends BaseAdapter {
+
+    private ArrayList<CustomDTO> listCustom = new ArrayList<>();
+
+    // ListView에 보여질 Item 수
+    @Override
+    public int getCount() {
+        return listCustom.size();
+    }
+
+    // 하나의 Item(ImageView 1)
+    @Override
+    public Object getItem(int position) {
+        return listCustom.get(position);
+    }
+
+    // Item의 id : Item을 구별하기 위한 것으로 position 사용
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    // 실제로 Item이 보여지는 부분
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        CustomViewHolder holder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_custom, null, false);
+
+            holder = new CustomViewHolder();
+            holder.imageView = (ImageView) convertView.findViewById(R.id.imageView);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (CustomViewHolder) convertView.getTag();
+        }
+
+        CustomDTO dto = listCustom.get(position);
+
+        holder.imageView.setImageResource(dto.getResId());
+
+        return convertView;
+    }
+
+    class CustomViewHolder {
+        ImageView imageView;
+    }
+
+    // MainActivity에서 Adapter에있는 ArrayList에 data를 추가시켜주는 함수
+    public void addItem(CustomDTO dto) {
+        listCustom.add(dto);
+    }
+}
